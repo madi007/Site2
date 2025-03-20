95c99c0fb2aa9c083c5324b2878e81a583a51df4
Zhanmyrza_0 <60744461+madi007@users.noreply.github.com>
2025-03-20 11:51:11 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -30,12 +30,35 @@ self.addEventListener("install", (e) => {
   );
 });
 
-self.addEventListener("activate", (e) => {
-  self.clients.claim();
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        console.log('Deleting old cache:', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request).catch(() => caches.match('idcard.html'));
+        })
+    );
 });
