cc78f8a0d6f43699edcd166f42a1298adeb0f9e7
Zhanmyrza_0 <60744461+madi007@users.noreply.github.com>
2025-03-20 10:16:49 +0500

    Update and rename appjs to app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('../service-worker.js') // sw.js — путь к твоему воркеру
+      navigator.serviceWorker.register('../service-worker.js')
         .then(reg => console.log('✅ Service Worker registered:', reg))
         .catch(err => console.error('❌ Service Worker registration failed:', err));
     });
