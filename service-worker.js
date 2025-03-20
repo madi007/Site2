7a7d8091e960bc8e0dd6ac5ae3b8e53bf35fa1e4
Zhanmyrza_0 <60744461+madi007@users.noreply.github.com>
2025-03-20 10:12:01 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,11 +4,26 @@ self.addEventListener("install", (e) => {
       cache.addAll([
         "./",
         "./index.html",
-        "./script.js",
-        "./style.css",
+        "./layouts/1.html",
+        "./layouts/2.html",
+        "./js/script.js",
+        "./js/1.js",
+        "./js/2.js",
+        "./js/app.js",
+        "./css/style.css",
+        "./css/1.css",
+        "./css/2.css",
+        "./css/all.css",
         "./manifest.json",
-        "./icon-192.png",
-        "./icon-512.png"
+        "./icon-message-192.png",
+        "./img/edit_icon.png"
+        "./icons/ic_avatar.svg"
+        "./icons/ic_back.svg"
+        "./icons/ic_delete.svg"
+        "./icons/ic_edit.svg"
+        "./icons/ic_info.svg"
+        "./icons/ic_more.svg"
+        "./icons/ic_send.svg"
       ])
     )
   );
