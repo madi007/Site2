801199f0efb5ab6744edbd874d95f77e1fdeed3e
Zhanmyrza_0 <60744461+madi007@users.noreply.github.com>
2025-03-23 17:02:20 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('../service-worker.js')
+      navigator.serviceWorker.register('service-worker.js')
         .then(reg => console.log('✅ Service Worker registered:', reg))
         .catch(err => console.error('❌ Service Worker registration failed:', err));
     });
